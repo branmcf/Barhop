67c32ea8ef84ed49d6a95dcb9cb1063bcf506efc
salam <sruthy.ks@techversantinfotech.com>
2017-03-15 16:35:16 +0530

    order management chnages

--- managed_account/admin.py
+++ managed_account/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import Trigger, MenuItems
+from models import Trigger, MenuItems, PurchaseOrder
 
 admin.site.register(Trigger)
 admin.site.register(MenuItems)
+admin.site.register(PurchaseOrder)
--- managed_account/urls.py
+++ managed_account/urls.py
@@ -6,7 +6,8 @@ from django.contrib.auth import views
 from . import views as account_view
 from .views import *
 
-urlpatterns = [
+urlpatterns = [ 
+    url(r'^$', account_view.account_home, name='accounts_details'),
     url(r'^account_status/$', account_view.account_status, name='account_status'),
     url(r'^users/$', UsersView.as_view(), name='user_view'),
     url(r'^banking/$', BankingView.as_view(), name='bank_view'),
@@ -14,4 +15,8 @@ urlpatterns = [
     url('^triggers/$', TriggerView.as_view(), name='triggers'),
     url(r'^edit_trigger/$', TriggerEditView.as_view(), name='edit_trigger'),
     url(r'^delete_trigger/(?P<pk>\d+)$', TriggerDeleteView.as_view(), name='delete_trigger'),
+    url(r'^order_ready/$', OrderReadyView.as_view()),
+    url(r'^order_close/$', OrderCloseView.as_view()),
+    url(r'^menu/$', MenuListView.as_view(), name='menu_view'),
+    # url(r'^add_menu_item/$', UsersView.as_view(), name='add_menu_item'),
 ]
