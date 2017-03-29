ee02e6b297598d2131ebb0c4298e0f4e16d4af48
rahul mr <rahul.mr@techversantinc.com>
2017-03-29 18:36:58 +0530

    Updating quantity of menu items on order cancellation

--- route/management/commands/close_conversation.py
+++ route/management/commands/close_conversation.py
@@ -9,7 +9,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 
-from managed_account.models import PurchaseOrder
+from managed_account.models import PurchaseOrder,OrderMenuMapping,MenuItems
 from route.models import Conversation
 from datetime import datetime, timedelta
 
@@ -19,12 +19,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         current_time = timezone.now()
-        time_threshold = current_time - timedelta(minutes=30)
+        time_threshold = current_time - timedelta(seconds=10)
         conversations = Conversation.objects.filter(date__lt=time_threshold,closed=False)
+
         for conversation in conversations:
             pending = 'PENDING'
+
             try:
                 order = PurchaseOrder.objects.get(conversation=conversation,order_status=pending)
+
+                # update MenuItems
+                orderMenuMappings = OrderMenuMapping.objects.filter(order=order)
+
+                for mapping in orderMenuMappings:
+                    quantity = mapping.quantity
+                    menu_item = mapping.menu_item
+                    new_quantity = menu_item.quantity_available + quantity
+                    menuItemObj = MenuItems.objects.get(id=menu_item.id)
+                    menuItemObj.quantity_available = new_quantity
+                    menuItemObj.save()
+
             except PurchaseOrder.DoesNotExist:
                 order = None
 
--- route/management/commands/close_expired_order.py
+++ route/management/commands/close_expired_order.py
@@ -5,11 +5,13 @@
 
 from django.core.management.base import BaseCommand
 from django.utils import timezone
-from managed_account.models import PurchaseOrder, GridDetails
+from managed_account.models import PurchaseOrder, GridDetails,OrderMenuMapping,MenuItems
 
 
 class Command(BaseCommand):
+
     def handle(self, *args, **options):
+
         orders = PurchaseOrder.objects.all()
 
         for order in orders:
@@ -29,6 +31,15 @@ class Command(BaseCommand):
                 griddetail.is_active = False
                 griddetail.save()
 
+                # update MenuItems
+                orderMenuMappings = OrderMenuMapping.objects.filter(order=order)
 
+                for mapping in orderMenuMappings:
+                    quantity = mapping.quantity
+                    menu_item = mapping.menu_item
+                    new_quantity = menu_item.quantity_available + quantity
+                    menuItemObj = MenuItems.objects.get(id=menu_item.id)
+                    menuItemObj.quantity_available = new_quantity
+                    menuItemObj.save()
 
 
